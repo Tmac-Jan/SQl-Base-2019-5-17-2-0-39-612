39ae8f4e8795dca74a716e1a59b98b0d1ce9488e
macmanboy <macmanboy@foxmail.com>
2019-07-03 20:06:04 +0800

    add data

--- db.sql
+++ db.sql
@@ -6,6 +6,9 @@ CREATE TABLE `student`(
 `age` tinyint NOT NULL,
 `sex` VARCHAR(2) NOT NULL,
 );
+INSERT INTO `student` VALUES ('001', '张三', 18, '男');
+INSERT INTO `student` VALUES ('002', '李四', 20, '女');
+
 CREATE TABLE `student`  (
   `id` varchar(32) CHARACTER NOT NULL,
   `name` varchar(32) CHARACTER NOT NULL,
@@ -13,8 +16,9 @@ CREATE TABLE `student`  (
   `sex` varchar(2) CHARACTER  NOT NULL,
   PRIMARY KEY (`id`)
 )
+INSERT INTO `subject` VALUES ('1001', '语文', '王老师', '本次考试比较简单');
+INSERT INTO `subject` VALUES ('1002', '数学', '刘老师', '本次考试比较难');
 
-SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE table `score` (
 `id` varchar(32) NOT NULL PRIMARY key,
@@ -24,3 +28,7 @@ CREATE table `score` (
 CONSTRAINT `FK_score_student_id` FOREIGN KEY(`student_id`) REFERENCES `student`(`id`) on delete cascade on update cascade
 );
 alter table `score` add FOREIGN KEY (`subject_id`) REFERENCES `subject`(`id`);
+INSERT INTO `score` VALUES (1, '001', '1001', 80.0);
+INSERT INTO `score` VALUES (2, '002', '1002', 60.0);
+INSERT INTO `score` VALUES (3, '001', '1001', 70.0);
+INSERT INTO `score` VALUES (4, '002', '1002', 60.5);
